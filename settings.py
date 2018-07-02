5ec84f250c466d99a258a8c25b118eb28cc8771b
msxf <mingxing.huang@msxf.com>
2018-07-02 12:01:09 +0800

    图片分组，高清模式

--- settings.py
+++ settings.py
@@ -1,5 +1,8 @@
-base_url = 'http://beauty.pcauto.com.cn'
-headers = {
+BASE_URL = 'http://beauty.pcauto.com.cn'
+HEADERS = {
             'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_5) AppleWebKit/537.36 (KHTML, like Gecko) '
                           'Chrome/67.0.3396.99 Safari/537.36'
           }
+
+# 图片模式：普通（group），高清（source）
+MODE = 'source'
